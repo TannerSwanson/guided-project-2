374b76921dbbae2b18445215c00220b15c09d47b
Tanner Swanson <TSWANSO3@travelers.com>
2025-07-01 09:52:37 -0400

    Fixed links, added css

--- react/src/components/Character.jsx
+++ react/src/components/Character.jsx
@@ -30,22 +30,22 @@ function Character() {
   const homeworldFinder = async (id) => {
     const resp = await fetch(`http://localhost:3000/api/planets/${id}`);
     const planets = await resp.json();
-    setHomeworld(planets.name);
+    setHomeworld(planets);
   }
 
   const filmFinder = async (id) => {
     const resp = await fetch(`http://localhost:3000/api/characters/${id}/films`);
-    const films = await resp.json();
-    const films_names = films.map(film => film.title);
-    setFilms(films_names);
+    const filmsObj = await resp.json();
+    setFilms(filmsObj);
+    console.log(filmsObj);
   }
 
   const goToFilmPage = (id) => {
-    navigate(`/character/${id}`);
+    navigate(`/film/${id}`);
   };
 
   const goToPlanetPage = (id) => {
-    navigate(`/character/${id}`);
+    navigate(`/planet/${id}`);
   };
 
   return (
@@ -59,11 +59,21 @@ function Character() {
             </section>
             <section id="planets">
             <h2>Homeworld</h2>
-            <p> {homeworld}</p>
+            <div id="charactersList">
+                <div key={homeworld.id} onClick={() => goToPlanetPage(homeworld.id)}>
+                    {homeworld.name}
+                </div>
+            </div>
             </section>
             <section id="films">
             <h2>Films appeared in</h2>
-            <ul>{films}</ul>
+            <div id="charactersList">
+                {films.map(film => (
+                <div key={film.id} onClick={() => goToFilmPage(film.id)}>
+                    {film.title}
+                </div>
+                ))}
+            </div>
             </section>
         </main>
     </div>
